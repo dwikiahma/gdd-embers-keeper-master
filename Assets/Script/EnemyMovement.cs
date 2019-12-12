c5041a2203574be50c8b006a9346703a469e5e1c
dwikiahma <dwikiahma@gmail.com>
2019-12-12 17:04:40 +0700

    first commit

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed=3;
+    public bool moveRight=false;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveRight){
+            transform.Translate(2*Time.deltaTime*speed,0,0);
+            transform.localScale= new Vector2(2,2);
+        }else{
+            transform.Translate(-2*Time.deltaTime*speed,0,0);
+            transform.localScale= new Vector2(-2,2);
+        }
+        animator.SetFloat("Speed",speed);
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.CompareTag("Turnback")){
+            if(moveRight){
+                moveRight=false;
+            }else{
+                moveRight=true;
+            }
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	float horizontalMove=0f;
+	public float runSpeed=40f;
+	bool Jump =false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove= Input.GetAxisRaw("Horizontal")*runSpeed;
+		animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+		if(Input.GetButtonDown("Jump")){
+			Jump = true;
+			animator.SetBool("IsJump",true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.CompareTag("DeadZone")){
+			Debug.Log("Mampozz");
+		}
+		
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag == "Enemy"){
+			Debug.Log("hit enemy");
+		}
+	}
+	
+
+	void FixedUpdate(){
+		controller.Move(horizontalMove*Time.fixedDeltaTime,false,Jump);
+		Jump=false;
+	}
+
+	public void OnLanding(){
+		animator.SetBool("IsJump",false);
+	}
+}
