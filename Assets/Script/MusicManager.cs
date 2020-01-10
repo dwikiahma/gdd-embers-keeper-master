442c227f77f95d46a0a23904029803fb51ce7012
dwikiahma <dwikiahma@gmail.com>
2020-01-09 17:02:06 -0800

    Music Game
    Game Music

---
+++ Assets/Script/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+	
+	private static MusicManager instance = null;
+	
+	public MusicManager Instance()
+	{
+		return instance;
+	}
+	
+	public void Awake()
+	{
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
